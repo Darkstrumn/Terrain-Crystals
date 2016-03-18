3dd04808ac7e3471dafebf04e55ea2d126f0a32f
Bailey Hollingsworth <baileyholl@gmail.com>
2016-03-17 22:43:58 -0500

    Begin abstraction

---
+++ src/main/java/com/DrasticDemise/TerrainCrystals/Items/TerrainCrystalAbstract.java
@@ -0,0 +1,173 @@
+package com.DrasticDemise.TerrainCrystals.Items;
+
+import java.util.ArrayList;
+
+import com.DrasticDemise.TerrainCrystals.ConfigurationFile;
+
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public abstract class TerrainCrystalAbstract extends Item{
+
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn){
+		return gatherBlockGenList(itemStackIn, worldIn, playerIn, 11, BiomeGenBase.plains);
+	}
+	public ItemStack gatherBlockGenList(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, int diameter, BiomeGenBase desiredBiome){
+		int blocksGenerated = 0;
+		if(!worldIn.isRemote){
+			int posX = MathHelper.floor_double(playerIn.posX); 
+			int posY = MathHelper.floor_double(playerIn.posY);
+			int posZ = MathHelper.floor_double(playerIn.posZ);
+			int center;
+			double radius = diameter/2.0;
+			if(diameter%2 != 0){
+				center = (int) (radius + 0.5);
+			}else{
+				center = (int) (radius);
+			}
+			int offsetXFirstHalf = (int) (posX + radius);
+			//Not sure why this has to be offset by 1 extra, but it does.
+			int offsetXSecondHalf = (int) (posX - radius + 1);
+			int yDown = 1;
+			int fakeCenter = center;
+			ArrayList<BlockPos> posList = new ArrayList<BlockPos>(68);
+			for(int i = 0; i < (fakeCenter); i ++){
+				//Creates a circle and fills it
+				for(int placeInwards = 0; placeInwards < i+1; placeInwards++){
+					//Fills across the circle
+					BlockPos fillShellOne = new BlockPos(offsetXFirstHalf - i, posY - yDown, posZ - i + placeInwards);
+					posList.add(fillShellOne);
+					BlockPos fillShellTwo = new BlockPos(offsetXFirstHalf - i, posY - yDown, posZ + i - placeInwards);
+					posList.add(fillShellTwo);
+				}
+			}
+			//Generates the second half
+			for(int i = 0; i < (center); i ++){
+				BlockPos shellThree = new BlockPos(offsetXSecondHalf + i, posY - 1, posZ  + i);
+				BlockPos shellFour = new BlockPos(offsetXSecondHalf + i, posY - 1, posZ - i);
+				posList.add(shellThree); 
+				posList.add(shellFour);
+				
+				for(int placeInwards = 0; placeInwards < i + 1; placeInwards++){
+					BlockPos fillShellThree = new BlockPos(offsetXSecondHalf + i, posY - 1, posZ + i - placeInwards);
+					BlockPos fillShellFour = new BlockPos(offsetXSecondHalf + i, posY - 1, posZ - i + placeInwards);
+					posList.add(fillShellThree);
+					posList.add(fillShellFour);
+				}
+			}
+			for(BlockPos p : posList){
+				blocksGenerated = generateSpike(posList, worldIn, playerIn, blocksGenerated, itemStackIn, desiredBiome);
+			}
+		}
+		//System.out.println(blocksGenerated);
+		//itemStackIn.damageItem(blocksGenerated, playerIn);
+		//System.out.println(itemStackIn.getItemDamage());
+		itemStackIn.damageItem(blocksGenerated, playerIn);
+		return itemStackIn;
+	}
+	public int generateSpike(ArrayList<BlockPos> posList, World worldIn, EntityPlayer playerIn, int blocksGenerated, ItemStack itemStackIn, BiomeGenBase desiredBiome){
+		ArrayList<BlockPos> recursiveList = new ArrayList<BlockPos>();
+		for(BlockPos pos : posList){
+			int surroundingBlocks = 0;
+			
+				blocksGenerated = generateInWorld(pos, worldIn, playerIn, blocksGenerated, desiredBiome);
+				
+				if(worldIn.getBlockState(pos.north()) != Blocks.air.getDefaultState()){
+					//System.out.println("entered northCheck");
+					surroundingBlocks++;
+				}
+				
+				if(worldIn.getBlockState(pos.east()) != Blocks.air.getDefaultState()){
+					surroundingBlocks++;
+				}
+				
+				if(worldIn.getBlockState(pos.south()) != Blocks.air.getDefaultState()){
+					surroundingBlocks++;
+				}
+				
+				if(worldIn.getBlockState(pos.west()) != Blocks.air.getDefaultState()){
+					surroundingBlocks++;
+				}
+				if(surroundingBlocks >= 3 || Math.random() < 0.05){
+					blocksGenerated = generateInWorld(pos.down(), worldIn, playerIn, blocksGenerated, desiredBiome);
+					recursiveList.add(pos.down());
+				}
+			}
+		if(!recursiveList.isEmpty()){
+			blocksGenerated = generateSpike(recursiveList, worldIn, playerIn, blocksGenerated, itemStackIn, desiredBiome);
+		}
+		return blocksGenerated;
+	}
+	private int generateInWorld(BlockPos pos, World worldIn, EntityPlayer playerIn, int blocksGenerated, BiomeGenBase desiredBiome){
+		if(worldIn.getBlockState(pos) == Blocks.air.getDefaultState()){
+			int posY = MathHelper.floor_double(playerIn.posY);
+			if(posY - pos.getY() == 1){
+				if(Math.random() < .7){
+					worldIn.setBlockState(pos, Blocks.sand.getDefaultState());
+					
+					if(ConfigurationFile.desertCrystalChangesBiome){
+						setBiome(worldIn, pos, desiredBiome);
+					}
+					
+					platformDecoration(worldIn, pos);
+				}else{
+					worldIn.setBlockState(pos, Blocks.sandstone.getDefaultState());
+				}
+			}else{
+				if(Math.random() < .9){
+					worldIn.setBlockState(pos, Blocks.sandstone.getDefaultState());
+				}else{
+					worldIn.setBlockState(pos, Blocks.sand.getDefaultState());
+				}
+			}
+			blocksGenerated++;
+		}
+		return blocksGenerated;
+	}
+	private void platformDecoration(World worldIn, BlockPos pos){
+		if(Blocks.cactus.canPlaceBlockAt(worldIn, pos.up()) && ConfigurationFile.desertCrystalGenerateCactus){
+			if(Math.random() < .10){
+				if(Math.random() < .5){
+					worldIn.setBlockState(pos.up(), Blocks.cactus.getDefaultState());
+					if(Math.random() < .5){
+						worldIn.setBlockState(pos.up(2), Blocks.cactus.getDefaultState());
+						if(Math.random() < .5){
+							worldIn.setBlockState(pos.up(3), Blocks.cactus.getDefaultState());
+						}
+					}
+				}else{
+					worldIn.setBlockState(pos.up(), Blocks.deadbush.getDefaultState());
+				}
+			}
+		}
+	}
+	//Code taken from World Edit by Skq89
+	//https://goo.gl/iEi0oU
+	public boolean setBiome(World worldIn, BlockPos position, BiomeGenBase desiredBiome) {
+        Chunk chunk = worldIn.getChunkFromBlockCoords(position);
+       // BiomeGenBase desiredBiome = BiomeGenBase.desert;
+        if ((chunk != null) && (chunk.isLoaded())) {
+        	if(worldIn.getChunkFromBlockCoords(position).getBiome(position, worldIn.getWorldChunkManager()).biomeID != desiredBiome.biomeID){
+        		chunk.getBiomeArray()[((position.getZ() & 0xF) << 4 | position.getX() & 0xF)] = (byte) desiredBiome.biomeID;
+	            return true;
+        	}
+        }
+        return false;
+    }
+	@SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+}
